b6d3c91636a9346499451bd45452f4d8017716f3
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-11-07 10:06:03 +0530

    Remove default React files and setup TailwindCSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#F1F1F6",
+        headingMain: "#BE9FE1",
+        headingSub: "#C9B6E4",
+        accent: "#E1CCEC",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        itim: ["Itim", "cursive"],
+      },
+      animation: {
+        "spin-slow": "spin 8s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
