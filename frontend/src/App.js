a7942826b047985ae90688a1c6e58029d34185de
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-11-07 10:20:01 +0530

    Setup frontend structure with basic components and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-const App = () => {
+function App() {
   return (
-    <div className="min-h-screen bg-bg text-headingMain font-bold text-center text-2xl">
-      App
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-};
+}
 
 export default App;
